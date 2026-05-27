6107b0c133ec651401fa3fc432d23f0ea6958161
jesusxal777-boop <jesusxal777@gmail.com>
2026-05-27 01:26:56 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 const SUPABASE_URL = "https://qynkgbgxavjgmvnskbsd.supabase.co";
 
 // Tu clave pública anon verificada y vinculada
-const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bmtnYmd4YXZqZ212bnNrYnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQ5NzMsImV4cCI6MjA5NTQyMDk3M30.2lbtOLD-pgKYYrJiNsaOIsw3oM3jaZUgccDA-68Y6JI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bmtnYmd4YXZqZ212bnNrYnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQ5NzMsImV4cCI6MjA5NTQyMDk3M30.2lbtOLD-pgKYYrJiNsaOIsw3oM3jaZUgccDA-68Y6JI";
 
 // Inicializamos el cliente de forma global en el navegador
 try {
